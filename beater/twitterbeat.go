65f30927b429e5bcbb64878f96d0ac5610c84089
Christoph Bühler <christoph@smartive.ch>
2016-02-24 17:03:15 +0100

    adding screen tweets and loop

--- beater/twitterbeat.go
+++ beater/twitterbeat.go
@@ -1,15 +1,16 @@
 package beater
 
 import (
-	"fmt"
-
 	"flag"
 	"github.com/ChimeraCoder/anaconda"
 	"github.com/buehler/PersistentStringMap/persistency"
 	"github.com/elastic/beats/libbeat/beat"
 	"github.com/elastic/beats/libbeat/cfgfile"
+	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/logp"
 	"github.com/elastic/beats/libbeat/publisher"
+	"net/url"
+	"strconv"
 	"time"
 )
 
@@ -20,7 +21,7 @@ type TwitterBeat struct {
 	api        *anaconda.TwitterApi
 	collecting bool
 	config     *TwitterConfig
-	events     *publisher.Client
+	events     publisher.Client
 	period     time.Duration
 	twitterMap *persistency.PersistentStringMap
 }
@@ -55,7 +56,7 @@ func (tb *TwitterBeat) Config(b *beat.Beat) error {
 
 func (tb *TwitterBeat) Setup(b *beat.Beat) error {
 	tb.alive = make(chan byte)
-	tb.events = &b.Events
+	tb.events = b.Events
 
 	anaconda.SetConsumerKey(*tb.config.Twitter.ConsumerKey)
 	anaconda.SetConsumerSecret(*tb.config.Twitter.ConsumerSecret)
@@ -101,7 +102,60 @@ func (tb *TwitterBeat) collectTweets() error {
 	defer func() {
 		tb.collecting = false
 	}()
-	fmt.Println("collecting.")
+
+	sync, err, processed := make(chan byte), make(chan error), 0
+
+	for _, name := range *tb.config.Twitter.Names {
+		go tb.processUser(name, sync, err)
+	}
+
+	for {
+		select {
+		case <-sync:
+			processed++
+			if processed == len(*tb.config.Twitter.Names) {
+				return nil
+			}
+		case e := <-err:
+			return e
+		}
+	}
 
 	return nil
+}
+
+func (tb *TwitterBeat) processUser(name string, sync chan byte, err chan error) {
+	var e error
+
+	v := url.Values{}
+	v.Set("screen_name", name)
+	v.Set("trim_user", "true")
+
+	if tb.twitterMap.Contains(name) {
+		v.Set("since_id", tb.twitterMap.Get(name))
+	}
+
+	result, e := tb.api.GetUserTimeline(v)
+
+	if e != nil {
+		err <- e
+		return
+	}
+
+	for _, tweet := range result {
+		event := common.MapStr{
+			"@timestamp": common.Time(time.Now()),
+			"type":       "tweet",
+			"screenName": name,
+			"tweet":      tweet,
+		}
+
+		tb.events.PublishEvent(event)
+	}
+
+	if len(result) >= 1 {
+		tb.twitterMap.Set(name, strconv.FormatInt(result[0].Id, 10))
+	}
+
+	sync <- 1
 }
